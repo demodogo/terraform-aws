d105ab548ddffb77fa321f4a7263a970fce481d4
demodogo <macaigm@gmail.com>
2025-01-03 19:35:03 -0300

    force delete ecr after workflow

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "ecr_repository" {
   name                 = var.ecr_name
   image_tag_mutability = "MUTABLE"
-
+  force_delete = true
 }
 
 resource "aws_ecr_repository_policy" "policy" {
