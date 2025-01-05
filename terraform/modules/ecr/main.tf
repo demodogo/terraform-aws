6b8d2348748bf3bef2632a7522a0747d4861999a
demodogo <macaigm@gmail.com>
2025-01-05 06:16:00 -0300

    manual ecr docker image scanning

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -2,6 +2,9 @@ resource "aws_ecr_repository" "ecr_repository" {
   name                 = var.ecr_name
   image_tag_mutability = "MUTABLE"
   force_delete = true
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
 
 resource "aws_ecr_repository_policy" "policy" {
