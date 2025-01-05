f6229eaabc517ed31d0bb63ef5642585bcd0f363
demodogo <macaigm@gmail.com>
2025-01-05 01:03:56 -0300

    test lambda func

---
+++ lambda_func/lambda.py
@@ -0,0 +1,20 @@
+import json
+import boto3
+import os
+
+client = boto3.client('sns')
+
+SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
+
+
+def lambda_handler(event, context):
+    for record in event["Records"]:
+        payload = record["body"]
+        try:
+            payload = json.loads(payload)
+            print(f"Mensaje recibido: {payload} ")
+            response = client.publish(TopicArn=SNS_TOPIC_ARN,Message="Mensajes de SQS procesados con éxito.")
+        except json.JSONDecodeError:
+            print(f"El mensaje no es un JSON válido: {payload}")
+
+        print(f"Payload procesado: {payload}")
