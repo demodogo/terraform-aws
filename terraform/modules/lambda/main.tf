6e37dc36413bf8e2c123a05702902c8f0dfa8e13
demodogo <macaigm@gmail.com>
2025-01-05 01:44:09 -0300

    fix:lambda function path

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,7 +1,7 @@
 # Comprimir archivo .py que tiene la función
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda_func"
+  source_dir  = "${path.root}/../lambda_func"
   output_path = "${path.module}/terraform/modules/lambda/lambda.zip"
 }
 
