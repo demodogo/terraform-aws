c6eb069e93b3a3b6f4e480811a9bc427444e2c68
demodogo <macaigm@gmail.com>
2025-01-06 02:45:12 -0300

    fix: fix lambda function to send sns response

--- lambda_func/lambda.py
+++ lambda_func/lambda.py
@@ -6,7 +6,6 @@ client = boto3.client('sns')
 
 SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
 
-
 def lambda_handler(event, context):
     for record in event["Records"]:
         payload = record["body"]
@@ -15,6 +14,7 @@ def lambda_handler(event, context):
             print("Mensaje recibido: " + str(payload))
             response = client.publish(TopicArn=SNS_TOPIC_ARN,Message="Mensajes de SQS procesados con éxito: " + str(payload))
             print("Noticación enviada con éxito")
+            return (response)
         except json.JSONDecodeError:
             print(f"El mensaje no es un JSON válido: {payload}")
 
