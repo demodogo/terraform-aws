b05b88c1d71ebf6904eeabf7871e3f86edf98d5d
demodogo <macaigm@gmail.com>
2025-01-06 08:34:11 -0300

    test ec2 workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ module "sg" {
 
 module "subnet" {
   source                    = "./modules/subnet"
-  vpc_id                    = module.vpc.vpc_id
+  vpc_id                    = var.vpc_id
   public_subnet_cidr_block  = var.public_subnet_cidr_block
   private_subnet_cidr_block = var.private_subnet_cidr_block
   public_subnet_name        = var.public_subnet_name
@@ -42,13 +42,13 @@ module "subnet" {
 
 module "igw" {
   source = "./modules/igw"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = var.vpc_id
   name   = var.gateway_name
 }
 
 module "route_table" {
   source              = "./modules/route_table"
-  vpc_id              = module.vpc.vpc_id
+  vpc_id              = var.vpc_id
   internet_gateway_id = module.igw.internet_gateway_id
   public_subnet_id    = module.subnet.public_subnet_id
   name                = var.public_route_table_name
