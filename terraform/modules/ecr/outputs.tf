a85a72e1a4268c0579a3a1dd460b2ff299694786
demodogo <macaigm@gmail.com>
2025-01-06 18:16:33 -0300

    run on main push

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -9,3 +9,7 @@ output "repository_url" {
 output "repository_registry_id" {
   value = aws_ecr_repository.ecr_repository.registry_id
 }
+
+output "repository_arn" {
+  value = aws_ecr_repository.ecr_repository.arn
+}
